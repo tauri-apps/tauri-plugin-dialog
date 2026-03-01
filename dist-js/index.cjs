65c00df5bb6619ed2c782c9cedf6d5533b646c48
Tony <68118705+Legend-Master@users.noreply.github.com>
2026-03-01 15:23:25 +0000

    refactor(dialog): handle `okLabel` in js side (#3295)
    * refactor(dialog): handle `okLabel` in js side
    
    * Allow unused instead of `cfg(desktop)`
    
    Committed via a GitHub action: https://github.com/tauri-apps/plugins-workspace/actions/runs/22546363291
    
    Co-authored-by: Legend-Master <Legend-Master@users.noreply.github.com>

--- dist-js/index.cjs
+++ dist-js/index.cjs
@@ -121,7 +121,6 @@ async function messageCommand(message, options) {
         message,
         title: options?.title,
         kind: options?.kind,
-        okButtonLabel: options?.okLabel,
         buttons: buttonsToRust(options?.buttons)
     });
 }
@@ -144,6 +143,9 @@ async function messageCommand(message, options) {
  */
 async function message(message, options) {
     const opts = typeof options === 'string' ? { title: options } : options;
+    if (opts && !opts.buttons && opts.okLabel) {
+        opts.buttons = { ok: opts.okLabel };
+    }
     return messageCommand(message, opts);
 }
 /**
--- dist-js/index.js
+++ dist-js/index.js
@@ -119,7 +119,6 @@ async function messageCommand(message, options) {
         message,
         title: options?.title,
         kind: options?.kind,
-        okButtonLabel: options?.okLabel,
         buttons: buttonsToRust(options?.buttons)
     });
 }
@@ -142,6 +141,9 @@ async function messageCommand(message, options) {
  */
 async function message(message, options) {
     const opts = typeof options === 'string' ? { title: options } : options;
+    if (opts && !opts.buttons && opts.okLabel) {
+        opts.buttons = { ok: opts.okLabel };
+    }
     return messageCommand(message, opts);
 }
 /**
